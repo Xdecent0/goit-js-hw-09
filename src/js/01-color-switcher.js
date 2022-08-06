aed2ec1ba647697bf002b100599387430e3cc37a
Xdecent <100188867+Xdecent0@users.noreply.github.com>
2022-08-06 16:02:27 +0300

    completed 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let isPressed = false;
+let timerId;
+// document.body.style.backgroundColor = 'yellow';
+startButton.addEventListener('click', () => {
+  if (!isPressed)
+    timerId = setInterval(() => {
+      isPressed = true;
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
 
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  isPressed = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
